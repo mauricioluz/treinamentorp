eab22de978b4289cdab374c11bceeaf0d00edde9
Mauricio barros <mau.barros@outlook.com>
2022-04-01 14:11:30 -0300

    alterações aula 5

--- lib/widgets/inputs/custom_text_input.dart
+++ lib/widgets/inputs/custom_text_input.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomTextInput extends StatelessWidget {
   final TextEditingController controller;
   final String labelText;
+  final String errorText;
+  final bool isRequired;
+  final bool isReadOnly;
+  final TextInputType inputType;
+  final TextCapitalization textCapitalization;
   const CustomTextInput(
-      {Key? key, required this.controller, required this.labelText})
+      {Key? key,
+      required this.controller,
+      required this.labelText,
+      this.errorText = "Campo obrigatório",
+      this.isRequired = false,
+      this.isReadOnly = false,
+      this.inputType = TextInputType.text,
+      this.textCapitalization = TextCapitalization.none})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
       controller: controller,
-      keyboardType: TextInputType.text,
-      textCapitalization: TextCapitalization.none,
-      readOnly: true,
+      keyboardType: inputType,
+      textCapitalization: textCapitalization,
+      readOnly: isReadOnly,
+      // maxLength: 20,
+      // maxLengthEnforcement: MaxLengthEnforcement.enforced,
+      validator: (text) {
+        if (isRequired) {
+          if (text!.isEmpty) {
+            return errorText;
+          }
+        }
+      },
       decoration: InputDecoration(
           labelText: labelText,
           labelStyle:
